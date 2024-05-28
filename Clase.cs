1688aefbbec2ed68983feb12296e6dae2620870b
Javier Villanueva <122271474+jvillanueva-iti@users.noreply.github.com>
2024-05-28 08:47:43 +0200

    Añadimos metodo de test

--- Clase.cs
+++ Clase.cs
@@ -2,4 +2,16 @@ public class Clase
 {
     public string nombre { get; set; }
     public int ID { get; set; }
+
+    public string test()
+    {
+        string result = "";
+        
+        for (int i = 0; i < 1000; i++)
+        {
+            result += i.ToString();
+        }
+
+        return "Clase";
+    }
 }
